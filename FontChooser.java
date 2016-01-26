91772ebcbac2a0de6bd1dd766ad98c3ba27b24e6
Maggie <mzyin@deerfield.edu>
2016-01-25 22:31:12 -0500

    code from canvas

---
+++ FontChooser.java
@@ -0,0 +1,17 @@
+import java.awt.GraphicsEnvironment;
+
+public class FontChooser
+{
+	   public static void main ( String [] args )
+	      {
+	      GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+	      String[] names = ge.getAvailableFontFamilyNames();
+	      for ( int i=0; i<names.length; i++ )
+	         {
+	             System.out.println( names[i] );
+	         }
+	      }
+	}
+
+
+
